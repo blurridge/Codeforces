14984ee18ddb2c11d3b14c97409783acbc17295b
Zach <zachriane01@gmail.com>
2021-11-27 11:42:58 +0800

    [C] Added solution for giftsFixing

---
+++ C/giftsFixing.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+long long minMoves(int numElements);
+
+int main(){
+
+    int cases, i, numElements;
+    long long result;
+    scanf("%d", &cases);
+    for(i = 0; i < cases; i++)
+    {
+        scanf("%d", &numElements);
+        result = minMoves(numElements);
+        printf("%lld\n", result);
+    }
+}
+
+long long minMoves(int numElements){
+    long long candies[numElements], oranges[numElements], candyDiff, orangeDiff, candyMin, orangeMin, movesMade = 0; 
+    int i;
+    for(i = 0; i < numElements; i++)
+    {
+        scanf("%lld", &candies[i]);
+        if(candies[i] < candyMin || i == 0)
+            candyMin = candies[i];
+    }
+    for(i = 0; i < numElements; i++)
+    {
+        scanf("%lld", &oranges[i]);
+        if(oranges[i] < orangeMin || i == 0)
+            orangeMin = oranges[i];
+    }
+    for(i = 0; i < numElements; i++)
+    {
+        candyDiff = candies[i] - candyMin;
+        orangeDiff = oranges[i] - orangeMin;
+        movesMade+=((candyDiff > orangeDiff) ? candyDiff : orangeDiff);
+    }
+    return movesMade;
+}
